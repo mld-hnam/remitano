8b59e05a62f696b2880af0a22c649b68f03f1c40
Hnam <nam.lam@finbase.vn>
2022-07-30 15:47:53 +0700

    chore: fix build

---
+++ build/static/js/977.55de0251.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_finbase_fincare=self.webpackChunk_finbase_fincare||[]).push([[977],{3977:function(e,n,t){t.r(n);t(2791);var i=t(8991),s=t(435),a=t(6871),c=t(4349),f=t(184);n.default=function(){var e=(0,a.s0)();return(0,f.jsx)(i.bW,{title:(0,f.jsx)(c.Z,{id:"page.503.title"}),content:(0,f.jsx)(c.Z,{id:"page.503.content"}),actions:(0,f.jsx)(s.Z,{type:"link",style:{fontWeight:"bold"},size:40,onClick:e(-1),children:(0,f.jsx)(c.Z,{id:"action.tryAgain"})})})}}}]);
+//# sourceMappingURL=977.55de0251.chunk.js.map
